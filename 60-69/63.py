bf6be8f6d288a14b5f2d6ab372874a12fc849a38
Benjamin Congdon <bencongdon96@gmail.com>
2015-12-01 10:25:06 -0600

    Refine 63 search space

--- 60-69/63.py
+++ 60-69/63.py
@@ -2,7 +2,7 @@ def num_digits(num):
 	digits = [int(i) for i in list(str(num))]
 	return len(digits)
 total = 0
-for b in range(1,1000):
+for b in range(1,10):
 	for e in range(1,100):
 		num = b**e
 		if num_digits(num) == e:
