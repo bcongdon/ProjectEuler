c527a8000fe48743c942d92b0055ae13dcf3f0c5
Benjamin Congdon <bencongdon96@gmail.com>
2015-12-01 10:23:02 -0600

    63

---
+++ 60-69/63.py
@@ -0,0 +1,11 @@
+def num_digits(num):
+	digits = [int(i) for i in list(str(num))]
+	return len(digits)
+total = 0
+for b in range(1,1000):
+	for e in range(1,100):
+		num = b**e
+		if num_digits(num) == e:
+			print str(b) + "^" + str(e) + " = " + str(num)
+			total += 1
+print "Total special numbers: " + str(total)
